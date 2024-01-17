8d3c514e4a3e15e7227dd00ea31d408a4f453d04
Nick O <ialwaysreply@gmail.com>
2024-01-16 23:10:39 -0700

    Initial commit

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,36 +1,56 @@
 import { useState } from 'react'
 import './Nav.css'
+import { art, blog, projects, placeHolder, placeHolder2 } from '../../data/data'
 
 export const Nav = () => {
     const [hover, setHover] = useState(false)
+    const [navButtonValue, setNavButtonValue] = useState('')
 
-    const changeHover = () => {
+    const changeHover = (e) => {
+        console.log(e.target.innerText);
         setHover(!hover)
+        setNavButtonValue(e.target.innerText)
     }
+
+    const renderModalItems = (modalArray) => {
+      return modalArray.map((item, index) => {
+        return (
+            <div key={index}>
+                <p>{item}</p>
+            </div>
+        )
+       })
+    }
+
+    const modalDescriptor = {
+        'Art' : art,
+        'Blog' : blog,
+        'Projects' : projects,
+        'Placeholder' : placeHolder,
+        'placeHolder2' : placeHolder2,
+    }
+
+
 
     return (
         <>
-     
+     <div className='nav-container'>
         <div>
            <ul className="nav-list">
-            <li  className='top-menu-nav-list-item' onMouseEnter={changeHover} onMouseLeave={changeHover}>Monsters</li>
-            <li className='top-menu-nav-list-item'>Aliens</li>
-            <li className='top-menu-nav-list-item'>Objects</li>
-            <li className='top-menu-nav-list-item'>Animals</li>
-            <li className='top-menu-nav-list-item'>Political</li>
+            <li className='top-menu-nav-list-item' onMouseEnter={(e)=> changeHover(e)}>Art</li>
+            <li className='top-menu-nav-list-item' onMouseEnter={(e)=> changeHover(e)}>Projects</li>
+            <li className='top-menu-nav-list-item' onMouseEnter={(e)=> changeHover(e)}>Blog</li>
+            <li className='top-menu-nav-list-item' onMouseEnter={(e)=> changeHover(e)}>Placeholder</li>
+            <li className='top-menu-nav-list-item' onMouseEnter={(e)=> changeHover(e)}>placeHolder2</li>
             
             </ul> 
             
         </div>
-        {hover && (
-            <div className={'dropdown-menu-center'}   onMouseEnter={changeHover} onMouseLeave={changeHover}>
-            <p>HI!</p>
-            <p>HI!</p>
-            <p>HI!</p>
-            <p>HI!</p>
-            <p>HI!</p>
-            <p>HI!</p>
+        {hover  && (
+            <div className={'dropdown-menu-center'} onMouseLeave={changeHover}>
+         {renderModalItems(modalDescriptor[navButtonValue])}
         </div> )}
+    </div>    
         </>
     )
 }
---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+export const art = [
+    'Monsters',
+    'Aliens',
+    'Political',
+    'Objects',
+]
+
+export const blog = [
+    '3d printing',
+    '3d modeling',
+    'General Complaining',
+    'Reviews',
+]
+
+export const projects = [
+    'Robots',
+    'Organization',
+    'Shop',
+]
+
+export const placeHolder = [
+    'blah1',
+]
+
+export const placeHolder2 = []
